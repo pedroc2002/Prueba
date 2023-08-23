86ca6979dba2d79b7438f8afb7c74b3b7aad98e6
Pedro Chavez <burrochavez1@gmail.com>
2023-08-22 22:09:34 -0400

    first commit

---
+++ cdp_web_python_oop_task-master/lib/shopping_app/item.py
@@ -0,0 +1,20 @@
+class Item:
+
+    from ownable import set_owner
+
+    instances = []
+
+    def __init__(self, name, price, owner=None):
+        self.name = name
+        self.price = price
+        self.set_owner(owner)
+        # Itemインスタンスの生成時、そのItemインスタンス(self)は、insntancesというクラス変数に格納されます。
+        Item.instances.append(self)
+
+    def label(self):
+        return {"name": self.name, "price": self.price}
+
+    @staticmethod
+    def item_all():
+        # instancesを返します ==> Item.item_all()でこれまでに生成されたItemインスタンスを全て返すということです。
+        return Item.instances
